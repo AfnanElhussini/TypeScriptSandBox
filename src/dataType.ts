ac5987922f849fe8cf2ffb1fdeb889032620c819
Afnan Elhussini <afnanrlhussini23@gmail.com>
2024-09-25 12:25:07 +0300

    Type Alies

--- src/dataType.ts
+++ src/dataType.ts
@@ -6,3 +6,28 @@ let myMName : str = "Afnan";
 type strAndNum = string | number;
 let myStrNmae : strAndNum = "Fanfona";
 let myAge :strAndNum  = 24;
+
+/* Advanced */
+
+type buttons = {
+    up: string,
+    right: string,
+    down: string,
+    left : string
+}
+
+function getActions(buttons){
+    console.log(`Action Button Up Is ${buttons.Up}`);
+    console.log(`Action Button Right Is ${buttons.right}`);
+    console.log(`Action Button Down Is ${buttons.down}`);
+    console.log(`Actions Button Left Is ${buttons.left}`)
+    
+    
+}
+
+getActions({
+    up: "jump",
+    right: "Go Right",
+    left: "Go LEFT",
+    dowm: "Go Down"
+})
