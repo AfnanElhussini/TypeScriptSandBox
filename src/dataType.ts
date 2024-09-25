2cf2620a5196c548c77c1c163a05d7732483df0f
Afnan Elhussini <afnanrlhussini23@gmail.com>
2024-09-25 12:16:28 +0300

    Data Type , Type Alies

---
+++ src/dataType.ts
@@ -0,0 +1,8 @@
+/* Data Types */ 
+/* Type Alies */
+type str = string;
+let myMName : str = "Afnan";
+
+type strAndNum = string | number;
+let myStrNmae : strAndNum = "Fanfona";
+let myAge :strAndNum  = 24;
